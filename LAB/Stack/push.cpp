6041c7ee5aee781a4ae36b1f87a01d4c993d7a70
UkashaZaheer04 <ukashazaheer.official@gmail.com>
2024-10-02 21:30:58 +0500

    with linklist

--- LAB/Stack/push.cpp
+++ LAB/Stack/push.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+/* #include<iostream>
 using namespace std;
 int stack[5] ,  top=-1, n=5;
 void push(int x)
@@ -62,4 +62,91 @@ int main ()
      pop();
      display();
   return 0;
+} */
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+    
+    Node(int value) {
+        data = value;
+        next = nullptr;
+    }
+};
+
+class Stack {
+private:
+    Node* top;  // Pointer to the top of the stack
+
+public:
+    Stack() {
+        top = nullptr;  // Stack is initially empty
+    }
+
+    bool isEmpty() {
+        return top == nullptr;  // Returns true if stack is empty
+    }
+
+    void push(int value) {
+        Node* newNode = new Node(value);
+        newNode->next = top;
+        top = newNode;
+        cout << value << " pushed to the stack." << endl;
+    }
+
+    int pop() {
+        if (isEmpty()) {
+            cout << "Stack underflow!" << endl;
+            return 0;
+        } else {
+            Node* temp = top;
+            top = top->next;
+            int poppedValue = temp->data;
+            delete temp;
+            return poppedValue;
+        }
+    }
+
+    int peek() {
+        if (isEmpty()) {
+            cout << "Stack is empty!" << endl;
+            return 0;
+        } else {
+            return top->data;
+        }
+    }
+
+    void display() {
+        if (isEmpty()) {
+            cout << "Stack is empty!" << endl;
+        } else {
+            Node* current = top;
+            cout << "Stack values are: ";
+            while (current != nullptr) {
+                cout << current->data << " ";
+                current = current->next;
+            }
+            cout << endl;
+        }
+    }
+};
+
+int main() {
+    Stack stack;
+    stack.push(10);
+    stack.push(20);
+    stack.push(30);
+
+    stack.display();
+
+    cout << "Top value: " << stack.peek() << endl;
+
+    stack.pop();
+    stack.display();
+
+    return 0;
 }
+
